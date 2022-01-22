2036574d9c9b61435f5e75439240d8587b91bd49
戴海洋 <1037306928@qq.com>
2022-01-22 19:23:16 +0800

    添加上传校验

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { LoginModule } from './login/login.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileModule } from './file/file.module';
 
 // 根模块
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, LoginModule], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
+  imports: [TypeOrmModule.forRoot(), UserModule, LoginModule, FileModule], // 导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
   controllers: [AppController], // 处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理
   providers: [AppService], // 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享
 })
