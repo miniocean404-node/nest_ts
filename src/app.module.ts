019717a3a3389366d5a56de4c9c13520b989c1d4
戴海洋 <1037306928@qq.com>
2022-01-24 18:35:06 +0800

    格式化

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,10 +19,10 @@ import { ExampleModule } from './example/example.module'
     UserModule,
     AuthModule,
     ExampleModule,
-  ], // 导入模块的列表，这些模块导出了此模块中所需提供者
+  ], // 导入其他模块中导出的Providers，这些模块导出了此模块中所需提供者，以实现共享
   controllers: [AppController, UserController], // 必须创建的一组控制器 处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理
-  providers: [], // 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享
-  exports: [], // 由本模块提供并应在其他模块中可用的提供者的子集。
+  providers: [], // 注入器实例化的提供者（服务提供者），处理具体的业务逻辑，模块内共享使用；
+  exports: [], // 导出其他模块需要共享的Providers
 })
 
 // 导出服务的列表，供其他模块导入使用。如果希望当前模块下的服务可以被其他模块共享，需要在这里配置导出；
