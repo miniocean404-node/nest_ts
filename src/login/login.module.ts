22a6ffa6d342b450c11ac04401b9887ec5fb31a4
戴海洋 <1037306928@qq.com>
2022-01-25 14:39:18 +0800

    inject属性注入

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common'
 import { LoginController } from './login.controller'
 import { LoginService } from './login.service'
-import { AuthService } from '../auth/auth.service'
 import { UserService } from '../user/user.service'
-import { JwtService } from '@nestjs/jwt'
 import { AuthModule } from '../auth/auth.module'
 
 @Module({
   controllers: [LoginController],
   providers: [LoginService, UserService],
   imports: [AuthModule],
-  exports: [LoginService],
 })
 export class LoginModule {}
