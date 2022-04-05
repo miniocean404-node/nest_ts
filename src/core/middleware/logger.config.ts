f1dc7ba1a90555d87ac33eca22e1eb7e1b82c7f0
MiniOcean404 <1037306928@qq.com>
2022-04-05 22:58:29 +0800

    修改logger.js文件的类型声明

--- src/core/middleware/logger.config.ts
+++ src/core/middleware/logger.config.ts
@@ -4,9 +4,16 @@
 // 字体背景颜色：black,red,green,yellow,blue, magenta,cyan,white,gray,grey
 // 背景颜色：blackBG,redBG,greenBG,yellowBG,blueBG,magentaBG,cyanBG,whiteBG
 
-import { createLogger, format, transports, addColors } from 'winston'
+import {
+	createLogger,
+	format,
+	transports,
+	addColors,
+	LoggerOptions,
+} from 'winston'
 import 'winston-daily-rotate-file'
 
+// 自定义日志等级颜色
 const myCustomLevels = {
 	levels: {
 		error: 0,
@@ -28,12 +35,20 @@ const myCustomLevels = {
 	},
 }
 
+addColors(myCustomLevels.colors)
+
+// 自定义格式化信息
 const customFormat = format.combine(
 	format.json(),
 	format.label({ label: `运行环境:dev` }),
 	format.timestamp({ format: 'MM-DD-YYYY HH:mm:ss' }),
 	format.align(),
-	format.printf((info) => `[${info.level}] \r\n时间：${[info['timestamp']]}\r\n载体:${info.message}`)
+	format.printf(
+		(info) =>
+			`[${info.level}] \r\n时间：${[info['timestamp']]}\r\n载体:${
+				info.message
+			}`,
+	),
 	// format.prettyPrint(),
 )
 
@@ -45,23 +60,27 @@ const defaultOptions = {
 	maxFiles: '14d',
 }
 
-addColors(myCustomLevels.colors)
-
-// 可以创建多个日志打印器
-export default {
+const customConfig: LoggerOptions = {
 	level: 'info',
 	levels: myCustomLevels.levels,
 	format: customFormat,
 	silent: false, // 是否禁用所有日志
 	transports: [
+		// 自定义控制台信息
 		new transports.Console({
 			level: 'info',
 			format: format.combine(
 				format.colorize(), // 开启控制台颜色展示
-				format.printf((info) => `[${info.level}] \r\n时间：${[info['timestamp']]}\r\n载体:${info.message}`)
+				format.printf(
+					(info) =>
+						`[${info.level}] \r\n时间：${[info['timestamp']]}\r\n载体:${
+							info.message
+						}`,
+				),
 			),
 			// level: 'warn',
 		}),
+		// 自定义文件输出配置
 		new transports.DailyRotateFile({
 			// handleExceptions:true //处理异常
 			// handleRejections:true //处理未经批准的拒绝承诺
@@ -76,6 +95,11 @@ export default {
 		}),
 	],
 	// exitOnError:false //异常时候是否退出 false 退出 默认为 true
-	exceptionHandlers: [new transports.File({ filename: 'logs/other/exceptions.log' })],
+	exceptionHandlers: [
+		new transports.File({ filename: 'logs/other/exceptions.log' }),
+	],
 	// rejectionHandlers: [new transports.File({ filename: 'logs/other/reject.log' })], //处理未经批准的拒绝承诺,
 }
+
+// 可以创建多个日志打印器
+export default customConfig
